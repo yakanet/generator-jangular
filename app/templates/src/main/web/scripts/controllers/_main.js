33ff7c7ebda78c90585e629042603ce6e5d5730e
yakanet <yakanet@gmail.com>
2013-11-23 00:20:09 +0800

    Remove globale variable

--- app/templates/src/main/web/scripts/controllers/_main.js
+++ app/templates/src/main/web/scripts/controllers/_main.js
@@ -1,14 +1,15 @@
 'use strict';
 
-var tempApp = angular.module('<%= _.camelize(baseName) %>App');
-tempApp.controller('MainCtrl', ['$scope', '$resource', function ($scope, $resource) {
-    $scope.awesomeThings = [
-        'HTML5 Boilerplate',
-        'AngularJS',
-        'Karma'
-    ];
+angular.module('<%= _.camelize(baseName) %>App')
 
-    $resource('rest/hello').get(function (d) {
-        $scope.info = d;
-    });
-}]);
+    .controller('MainCtrl', ['$scope', '$resource', function ($scope, $resource) {
+        $scope.awesomeThings = [
+            'HTML5 Boilerplate',
+            'AngularJS',
+            'Karma'
+        ];
+
+        $resource('rest/hello').get(function (d) {
+            $scope.info = d;
+        });
+    }]);
